639d56cd46ab4cb47ea2907f24f038cca5ba76fe
Hein Ko Zin <heinkozin4@gmail.com>
2022-12-23 11:57:01 +0630

    design layout added

---
+++ resources/views/pages/dashobard/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.mainlayout')
+@section('title', 'Dashboard')
+@section('content')
+    <div class="row">
+        <div class="col-md-12 grid-margin">
+            <h1>hi</h1>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(["namespace" => "Dashboard"], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
